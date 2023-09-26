2470f7b25a85e74acaed2c072164d931c153ac35
lucas <lucasturschwell@hotmail.com>
2023-09-26 13:21:12 +0200

    user_challenge entity creation

---
+++ src/Entity/UserChallenge.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserChallengeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserChallengeRepository::class)]
+class UserChallenge
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $statut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_fin = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_fin_obligatoire = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->date_fin;
+    }
+
+    public function setDateFin(\DateTimeInterface $date_fin): static
+    {
+        $this->date_fin = $date_fin;
+
+        return $this;
+    }
+
+    public function getDateFinObligatoire(): ?\DateTimeInterface
+    {
+        return $this->date_fin_obligatoire;
+    }
+
+    public function setDateFinObligatoire(\DateTimeInterface $date_fin_obligatoire): static
+    {
+        $this->date_fin_obligatoire = $date_fin_obligatoire;
+
+        return $this;
+    }
+}
