272989d2c74c2355668fcc821373eda6e01f3c03
JeffY1710 <jeff.yabas@gmail.com>
2023-09-27 16:49:04 +0200

    admin test

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,9 +15,9 @@ class SecurityController extends AbstractController
         if ($this->getUser()) {
             return $this->redirectToRoute('app_home');
         }
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+         if ($this->getUser()) {
+             return $this->redirectToRoute('app_home');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
